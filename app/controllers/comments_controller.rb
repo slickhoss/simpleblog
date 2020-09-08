582b43f99bf1dfb2f47018ae66001355298ab42d
Slickhoss <hoongandre@gmail.com>
2020-09-07 23:27:16 -0700

    commit #2

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+    http_authenticate_with name: "slickhoss", password: "12345", only: [:destroy]
+    def create
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.create(comment_params)
+        redirect_to post_path(@post)
+    end
+
+    def destroy
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+        @comment.destroy
+        redirect_to post_path(@post)
+    end
+
+    private def comment_params
+        params.require(:comment).permit(:username, :body)
+    end
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,51 @@
 class PostsController < ApplicationController
+    http_basic_authenticate_with name: "slickhoss", password: "12345", except: [:index, :show]
     def index 
-
+        #store all obj in the Post.model
+        @posts = Post.all
     end
-    
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
+    def new 
+        @post = Post.new
+    end
+
+    def create
+        #create new post obj from Post.model
+        @post = Post.new(post_params);
+
+        #save new post obj to database
+       if(@post.save)
+            redirect_to @post #redirect to the submitted post
+       else
+        render 'new'
+       end
+    end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+        if(@post.update(post_params))
+            redirect_to @post #redirect to the submitted post
+       else
+        render 'edit'
+       end
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_path
+    end
+
+    #define structure for params
+    private def post_params
+        params.require(:post).permit(:title, :body)
+    end
 end
